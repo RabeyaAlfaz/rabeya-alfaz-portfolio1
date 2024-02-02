c8f4fbdef67295384b95fe2eb23c0b90b2da913e
rabeyaalfaz <rabeyaalfaz3@gmail.com>
2024-02-02 15:30:26 +0600

    api

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,33 +4,46 @@ import {motion,useScroll,useSpring, useTransform} from 'framer-motion';
 const items  = [
   {
   id:1,
-  title:"React Commerce",
-  img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgIF-BInCbenZa1RTthWeYFxXv7R7WSR1sDQ&usqp=CAU",
-  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem."
+  title:"Speed Type",
+  img:"https://i.postimg.cc/wjpyrJck/speed-Type.png",
+  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem.",
+  link:"https://reverent-borg-f6d302.netlify.app/"
 },
   {
   id:2,
-  title:"ecommerce with next js",
-  img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQSb_d0arDC3vW3zb-uBGCjV0yoPYZMmOdpA&usqp=CAU",
-  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem."
+  title:"ecommerce with js",
+  img:"https://i.postimg.cc/ZnDmpyDC/ecommerce.png",
+  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem.",
+  link:"https://first-e-commerce-site.netlify.app/"
 },
   {
   id:3,
-  title:"commerce with vanilla css and js",
-  img:"https://images.fineartamerica.com/public/images/overview/homeDecor/throwPillows002.jpg",
-  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem."
+  title:"news Bulletin with React",
+  img:"https://i.postimg.cc/L83yWM2V/bulletin.png",
+  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem.",
+  link:"https://bulletin-today.netlify.app/"
 },
   {
   id:4,
-  title:"Dragon news with react",
-  img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWOiOe1Uz4FnSyvBxEeSEOb33O4apLnKqutPZcOFIVEjEbECApUE7-_YCOvriE7lzYQm0&usqp=CAU",
-  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem."
+  title:"Currency master With Js",
+  img:"https://i.postimg.cc/8zfpdtGD/currency-Master.png",
+  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem.",
+  link:"https://roaring-parfait-0b029e.netlify.app/"
 },
   {
   id:5,
-  title:"ecommerce with backend",
-  img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgIF-BInCbenZa1RTthWeYFxXv7R7WSR1sDQ&usqp=CAU",
-  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem."
+  title:"World Cup Update",
+  img:"https://i.postimg.cc/J4GMmPGJ/worldCup.png",
+  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem.",
+  link:"https://world-cup-update.netlify.app/"
+}
+,
+  {
+  id:6,
+  title:"Convention Center",
+  img:"https://i.postimg.cc/9Q046djN/convention-center.png",
+  desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore ipsum assumenda quas qui unde at eligendi nulla omnis, earum in voluptates vitae eos, veritatis optio maxime debitis distinctio tempora voluptatem.",
+  link:"https://reverent-borg-f6d302.netlify.app/"
 }
 ]
 const Single = ({item}) =>{
@@ -49,7 +62,7 @@ const Single = ({item}) =>{
     <motion.div className="textContainer" style={{y}}>
       <h2>{item.title}</h2>
       <p>{item.desc}</p>
-      <button>see demo</button>
+      <button><a href={img.link}>see demo</a></button>
     </motion.div>
   </div>
   </div>
